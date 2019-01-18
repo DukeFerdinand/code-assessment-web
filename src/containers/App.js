4f04469feb5eca1bb4b0d61c1f749530053ee457
Douglass Hooker <DouglassHooker@gmail.com>
2019-01-18 11:44:58 -0500

    Add basic app container style

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 import ProductsContainer from './ProductsContainer'
 import CartContainer from './CartContainer'
 
+// App level stylings
 import '../styles/main.scss'
 
+// Component Specific style
+import '../styles/components/App.scss'
+
 const App = () => (
-  <div>
-    <h2>Acme Store</h2>
+  <div className="app-container">
+    <h2 className="app-title">Acme Store</h2>
     <hr/>
     <ProductsContainer />
     <hr/>
