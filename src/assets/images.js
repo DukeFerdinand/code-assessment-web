a28f50d55f3a78ea3fae29c8a74cfcbd93ebb87e
Douglass Hooker <DouglassHooker@gmail.com>
2019-01-22 11:28:59 -0500

    Add thumbnail assets

---
+++ src/assets/images.js
@@ -0,0 +1,21 @@
+import Chronograph from './Chronograph.png'
+import Quartz from './Quartz.png'
+import Weekender from './Weekender.png'
+import NotFound from './NotFound.jpg'
+
+// Smaller versions
+import ChronographThumb from './thumbnails/Chronograph.jpeg'
+import QuartzThumb from './thumbnails/Quartz.jpeg'
+import WeekenderThumb from './Weekender.png'
+
+export default {
+  thumbnails: {
+    ChronographThumb,
+    QuartzThumb,
+    WeekenderThumb
+  },
+  Chronograph,
+  Quartz,
+  Weekender,
+  NotFound
+}
