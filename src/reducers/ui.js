09a57587488753999b4217d58316ddbe86446643
Douglass Hooker <DouglassHooker@gmail.com>
2019-01-23 18:38:07 -0500

    Add attempt at preventing scroll while modal is open

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -11,6 +11,7 @@ export const getModalStatus = (state) => state.modalOpen
 export function ui(state = initialState, action) {
   switch (action.type) {
     case TOGGLE_CART_MODAL:
+      document.body.style.overflow = action.modalOpen ? 'hidden' : 'initial'
       return { modalOpen: action.modalOpen }
     default:
       return state
