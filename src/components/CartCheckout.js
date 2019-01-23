18577bd2d09d97e6cc7e5a4c2fe296ef88caf2d4
Douglass Hooker <DouglassHooker@gmail.com>
2019-01-23 09:51:50 -0500

    Add test for CartCheckout

--- src/components/CartCheckout.js
+++ src/components/CartCheckout.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import '../styles/components/CartCheckout.scss'
 
 export default class CartCheckout extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.totals = this.findTax(props.total)
+  }
   findTax(total) {
     return {
       subtotal: total,
@@ -13,22 +19,22 @@ export default class CartCheckout extends React.Component {
     }
   }
   render() {
-    const { total } = this.props
+    const { subtotal, tax, total } = this.totals
     return (
       <div className="checkout-wrapper">
         <hr className="divider" />
         <div className="checkout-row">
           <p className="checkout-row-title">Subtotal</p>
-          <p className="checkout-row-price">&#36;{total}</p>
+          <p className="checkout-row-price checkout-subtotal">&#36;{subtotal}</p>
         </div>
         <div className="checkout-row">
           <p className="checkout-row-title">Tax</p>
-          <p className="checkout-row-price">&#36;{this.findTax(total).tax}</p>
+          <p className="checkout-row-price checkout-tax">&#36;{tax}</p>
         </div>
         <hr className="divider" />
         <div className="checkout-row">
           <p className="checkout-row-title">Total</p>
-          <p className="checkout-row-price">&#36;{this.findTax(total).total}</p>
+          <p className="checkout-row-price checkout-total">&#36;{total}</p>
         </div>
         <div className="checkout-row checkout-row-final">
           <button className="checkout-button" onClick={this.props.onCheckoutClicked}>
@@ -39,4 +45,9 @@ export default class CartCheckout extends React.Component {
     )
 
   }
+}
+
+CartCheckout.PropTypes = {
+  total: PropTypes.number.isRequired,
+  onCheckoutClicked: PropTypes.func.isRequired
 }
